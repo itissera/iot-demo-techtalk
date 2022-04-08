03ada3da478210bd75509cf171b9308c67abe0f9
itissera <iota@gft.com>
2022-04-08 11:06:27 -0600

    Adding some default values to some vars

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,16 +1,19 @@
 variable "project_name" {
    description = "The project ID where all resources will be launched."
-  type = string
+   type = string
+   default = "iot-lab-demo-346417"
 }
 
 variable "region" {
   description = "The location region to deploy the Cloud IOT services. Note: Be sure to pick a region that supports Cloud IOT."
   type        = string
+  default     = "us-central1"
 }
 
 variable "zone" {
   description = "The location zone to deploy the Cloud IOT services. Note: Be sure to pick a region that supports Cloud IOT."
   type        = string
+  default     = "us-central1"
 }
 
 variable "sendgrid_api_key" {
